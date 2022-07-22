dbaecb1beda47c3de3ed474b5724fe34f166ecd9
Abhinav Chaturvedi <73028294+Abhinav-Chaturvedi-095@users.noreply.github.com>
2022-07-22 09:51:55 +0530

    Create Nth Root Of M.cpp

---
+++ Nth Root Of M.cpp
@@ -0,0 +1,26 @@
+double multiply(double number, int n) {
+    double ans = 1.0;
+    for(int i = 1;i<=n;i++) {
+        ans = ans * number;
+    }
+    return ans; 
+}
+
+double findNthRootOfM(int n, long long m) {
+	// Write your code here.
+    double low = 1;
+    double high = m;
+    double eps = 1e-9; 
+    
+    while((high - low) > eps) {
+        double mid = (low + high) / 2.0; 
+        if(multiply(mid, n) < m) {
+            low = mid; 
+        }
+        else {
+            high = mid; 
+        }
+    }
+    
+    return low; 
+}
